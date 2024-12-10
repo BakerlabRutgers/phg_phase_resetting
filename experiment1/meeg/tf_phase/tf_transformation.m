182db3d1a114b7ef8224155670a25196855ce2cc
Malte Rudo Güth <maltegueth@gmail.com>
2024-12-10 12:43:34 -0600

    Update tf_transformation.m

--- experiment1/meeg/tf_phase/tf_transformation.m
+++ experiment1/meeg/tf_phase/tf_transformation.m
@@ -37,41 +37,41 @@ for s = 1:length(subs)
         
         for ci=1:chanNum
               
-          	chanVariable=chanList{ci}; % change channel names when switching between EEG and MEG
-          	POW_subj=zeros(1,60,TIME); %subject, frequency, data points
-          	POW_BASE_subj = zeros(1,60,TIME);
-          
-          	disp(strcat('Start processing for Channel:',chanVariable));
-                  
-          	fName = files(1).name;
-          	tmp=load ([pathdata, files(fi).name]);
-          	chanData=tmp.epochs;
-          	chanVariable=chanListeeg64{ci};
-          	tmpchan=reshape(chanData(ci,:,:),[TIME,size(chanData,3)])';
-          	POW = zeros(60,TIME);% freq, data points
-          
-          	for k=1:size(tmpchan,1)
-            		COEFS = cwt (tmpchan(k,:),SRate*1.5./Freq,'cmor1-1.5');
-            		POW = POW + abs (COEFS(:,1:TIME)).^2; 
-          	end
-          
-          	POW_subj(1,:,:)=POW;          
-          
-          	BASE=squeeze(mean(POW_subj(1,:,575:600),3)); %size of baseline -200--100 
-          			                              % MEGEEG: 1380-1440
-          			                              % EEG64: 575-600
-          	BASE=repmat(BASE',1,TIME);
-          	POW_BASE_subj(1,:,:)=(squeeze(POW_subj(1,:,:))-BASE)./BASE;  
-    
-            disp(strcat('done for suject',fName));
-            
-            save([pathout 'POWtotal_', conName, '_',chanVariable,'.mat'], 'POW_BASE_subj') % save complex data
-            POWtot = POW_BASE_subj;
-    
-            %%%%induced 
-            disp(strcat('Start induced for Channel:',chanVariable));
-            POW_subj=zeros(1,60,TIME); % subject, frequency, data points
-            POW_BASE_subj = zeros(1,60,TIME);
+		chanVariable=chanList{ci}; % change channel names when switching between EEG and MEG
+		POW_subj=zeros(1,60,TIME); %subject, frequency, data points
+		POW_BASE_subj = zeros(1,60,TIME);
+	  
+		disp(strcat('Start processing for Channel:',chanVariable));
+		  
+		fName = files(1).name;
+		tmp=load ([pathdata, files(fi).name]);
+		chanData=tmp.epochs;
+		chanVariable=chanListeeg64{ci};
+		tmpchan=reshape(chanData(ci,:,:),[TIME,size(chanData,3)])';
+		POW = zeros(60,TIME);% freq, data points
+	  
+		for k=1:size(tmpchan,1)
+			COEFS = cwt (tmpchan(k,:),SRate*1.5./Freq,'cmor1-1.5');
+			POW = POW + abs (COEFS(:,1:TIME)).^2; 
+		end
+	  
+		POW_subj(1,:,:)=POW;          
+	  
+		BASE=squeeze(mean(POW_subj(1,:,575:600),3)); %size of baseline -200--100 
+							      % MEGEEG: 1380-1440
+							      % EEG64: 575-600
+		BASE=repmat(BASE',1,TIME);
+		POW_BASE_subj(1,:,:)=(squeeze(POW_subj(1,:,:))-BASE)./BASE;  
+		
+		disp(strcat('done for suject',fName));
+		
+		save([pathout 'POWtotal_', conName, '_',chanVariable,'.mat'], 'POW_BASE_subj') % save complex data
+		POWtot = POW_BASE_subj;
+		
+		%%%%induced 
+		disp(strcat('Start induced for Channel:',chanVariable));
+		POW_subj=zeros(1,60,TIME); % subject, frequency, data points
+		POW_BASE_subj = zeros(1,60,TIME);
 
           
           	fName = files(1).name;
@@ -97,12 +97,12 @@ for s = 1:length(subs)
           	disp(strcat('done for suject',fName));
 
     
-            save([pathout 'POWinduced_', conName, '_',chanVariable,'.mat'], 'POW_BASE_subj') % save complex data
-    
-            POWind = POW_BASE_subj;
-            POW_evoked = POWtot-POWind;
-            
-            save([pathout 'POWevoked_', conName, '_',chanVariable,'.mat'], 'POW_evoked'); % save complex data
+		save([pathout 'POWinduced_', conName, '_',chanVariable,'.mat'], 'POW_BASE_subj') % save complex data
+		
+		POWind = POW_BASE_subj;
+		POW_evoked = POWtot-POWind;
+		
+		save([pathout 'POWevoked_', conName, '_',chanVariable,'.mat'], 'POW_evoked'); % save complex data
 
         end  %%%for each channel
 
